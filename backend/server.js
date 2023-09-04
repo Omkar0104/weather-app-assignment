aafd97d171128809f00b7caf62d01e256bb72ad6
Omkar0104 <omkarsonawane159@gmail.com>
2023-09-04 19:25:50 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 // const fetch = require("node-fetch");
+const axios = require('axios');
 
 const app = express();
 
-// Enable CORS for all routes
 app.use(cors());
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Define a POST endpoint to fetch weather data
 app.post("/getWeather", async (req, res) => {
   try {
-    // Retrieve the list of cities from the request body
     const cities = req.body;
-    console.log(cities);
+    // console.log(cities);
 
-    // Fetch weather data for each city (replace with your API)
     const weatherData = await Promise.all(
       cities.map(async (city) => {
         const apiKey = "06eb48fc720345238ea63202230409";
-        const response = await fetch(
-          `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
-        );
-        const data = await response.json();
+        // const response = await fetch(
+        //   `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+        // );
+         const response = await axios.get(
+           `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+         );
+        //  console.log(response.data);
+        const data =  response.data;
         // return data;
         return { [city]: data.current.temp_c + "°C" };
       })
     );
 
-    // Prepare the response
+    
     const weatherResponse = { weather: {} };
     weatherData.forEach((data) => {
       Object.assign(weatherResponse.weather, data);
